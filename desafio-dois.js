cc73d0cbba2c9361387798e620a88e77f0b471a3
Maria Eduarda de Oliveira Garcia <ol.mariae@outlook.com>
2022-01-23 20:33:31 -0300

    desafio dois

---
+++ desafio-dois.js
@@ -0,0 +1,42 @@
+
+var age = 30;
+
+if (age >= 18) {
+  console.log('Você já pode se sustentar melhor!');
+}
+
+var age = 25;
+if (age>= 18)
+var iAmHuman = true;
+
+if (iAmHuman) {
+  console.log('Se você é humano e tem 18 anos, vote.');
+}
+
+var janeiro = true;
+
+if (janeiro) {
+  console.log('Se você faz aniversário em Janeiro, oi!');
+} else {
+    console.log('Se não faz, tchau!')
+  }
+
+var dezembro = true;
+if (dezembro){
+    console.log('Se você faz aniversário em Dezembro, oi!')
+} else {
+    console.log('Se não faz, tchau!')
+  }
+  var r = true;
+  if(r){
+      console.log('Se seu nome começa com R, ria!')
+  } else{
+      console.log('Caso não comece com R, ria mesmo assim!')
+  }
+
+  var seise= true;
+  if (seise){
+      console.log('Se seu sobrenome tem seis letras ou começa com E, eba!')
+  } else{
+      console.log('Caso não, tudo bem!')
+  }
